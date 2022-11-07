7f8eca1acdaf2f26959d9bed25024d7fbe369f31
GowdhamHatake <gowdhamandrewssingh@gmail.com>
2022-11-07 19:44:58 +0530

    First time initiate

---
+++ src/test/java/ecom/boss/First.java
@@ -0,0 +1,20 @@
+package ecom.boss;
+
+public class First {
+	
+private void one() {
+
+	System.out.println(4);
+}
+
+private void two() {
+
+	System.out.println(6);
+}
+
+private void three() {
+
+	System.out.println(8);
+}
+
+}
