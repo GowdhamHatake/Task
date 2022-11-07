e6064ca77d45571996ec5cf71f06469c0694aebe
GowdhamHatake <gowdhamandrewssingh@gmail.com>
2022-11-07 20:56:44 +0530

    from John

--- src/test/java/ecom/boss/First.java
+++ src/test/java/ecom/boss/First.java
@@ -17,4 +17,19 @@ private void three() {
 	System.out.println(8);
 }
 
+private void four() {
+
+	System.out.println(10);
+}
+
+private void five() {
+
+	System.out.println(12);
+}
+
+private void six() {
+
+	System.out.println(14);
+}
+
 }
